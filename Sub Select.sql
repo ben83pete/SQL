db29941f6f9b66e5ad6e2156b88f2b8c625ee3eb
ben Petersen <ben83pete@gmail.com>
2019-01-24 09:23:54 -0500

    corrected sql

--- Sub Select.sql
+++ Sub Select.sql
@@ -1,12 +1,6 @@
-<<<<<<< HEAD
+
 -- display individual customers w/ sales > the AVG of all Customers
 -- select avg(sales) from customers;
 
 select * from customers
-=======
--- display individual customers w/ sales > the AVG of all Customers
--- select avg(sales) from customers;
-
-select * from customers
->>>>>>> 07002971464df74993c24ca04285c1e1545c8c27
 where sales > (select avg(sales) from customers)
